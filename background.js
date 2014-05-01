4ee9d51bdd8f4b843795b4566404ec173019df95
Tomoo Mizukami <tomoo.mizukami@sonymobile.com>
2014-05-01 14:16:23 +0900

    remove jquery

--- background.js
+++ background.js
@@ -6,12 +6,12 @@ function enable(info, tab) {
 }
 
 var copyTextToClipboard = function(txt){
-        var copyArea = $("<textarea/>");
-        copyArea.text(txt);
-        $("body").append(copyArea);
-        copyArea.select();
-        document.execCommand("copy");
-        copyArea.remove();
+    var copyArea = document.createElement("textarea");
+    copyArea.value = txt;
+    document.body.appendChild(copyArea);
+    copyArea.select();
+    document.execCommand("copy");
+    document.body.removeChild(copyArea);
 }
 
 function doCopy(info, tab) {
